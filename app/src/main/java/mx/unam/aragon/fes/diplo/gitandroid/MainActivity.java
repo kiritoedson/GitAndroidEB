adc3e8dcf5004340ab33e159a1aca7ca43d53d4f
kiritoedson <edson_beatle@hotmail.com>
2018-02-10 13:27:33 -0600

    commint inicial de un proyecto android

--- app/src/main/java/mx/unam/aragon/fes/diplo/gitandroid/MainActivity.java
+++ app/src/main/java/mx/unam/aragon/fes/diplo/gitandroid/MainActivity.java
@@ -2,12 +2,26 @@ package mx.unam.aragon.fes.diplo.gitandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button boton;
+    private TextView txtView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        boton=findViewById(R.id.button);
+        txtView=findViewById(R.id.textView);
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtView.setText("Hola GitHub");
+            }
+        });
     }
 }
